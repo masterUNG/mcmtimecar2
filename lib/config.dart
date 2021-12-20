4a929ba266dc79ed0645c3deaeaead60bed345c9
pegmongkol <pegmongkol@gmail.com>
2021-12-20 09:30:49 +0700

    update

---
+++ lib/config.dart
@@ -0,0 +1,10 @@
+class AppConfig {
+  // final String url = "http://192.168.1.41/mcm/";
+  // final String url = "https://mcmchemical.com/";
+  final String url = "http://178.128.113.206/";
+  // final String url = "http://192.168.1.41/mcmpsql/";
+  Map<String, String> header = {
+    'Content-Type': 'application/json-patch+json',
+    'accept': 'application/json'
+  };
+}
